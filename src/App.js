fc9756cc2d2fb25885ed9413c3c0f4c9530765f5
unknown <sunnyvivi3@gmail.com>
2020-04-21 11:22:05 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css';
+import {createAction, deleteAction, editAction} from './actions';
+import {selectAllNotes} from './selectors';
+
+
+function App() {
+  const todos = useSelector(selectAllNotes);
+  const dispatch = useDispatch();
+  const [todo, setTodo] = useState('First todo');
+
+  const addTodo = (text) => {
+    dispatch(createAction({text}));
+    setTodo('')
+  }
+
+  const editTodo = (item) => {
+    dispatch(editAction(item))
+  }
+  
+  const deleteTodo = (id) => dispatch(deleteAction(id))
+
+  return (
+    <div className="App">
+      <div className="add-todo">
+        <h2>Add todo</h2>
+        <input type="text" className="add-todo-input" 
+        value={todo} onChange={e => setTodo(e.target.value)}/>
+        <button className="add-todo-add" onClick={() => addTodo(todo)}>Add</button>
+      </div>
+      <div className="todo-list">
+        {
+          todos.map((item, index) => {
+            return (
+              <div className="todo-item" key={item.id}>
+                <span>{index+1}</span>
+                <input type="text" className="change-todo-input" 
+                value={item.text} onChange={e => editTodo({text:e.target.value, id: item.id })} />
+                <button className="delete-todo" onClick={() => deleteTodo(item.id)}
+                >X</button>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
